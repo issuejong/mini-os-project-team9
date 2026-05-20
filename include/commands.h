f3a8afaeb5db9660124a76f3d6b5abf05f41e6ab
juheon <wngjs8114@gmail.com>
2026-05-20 17:57:58 +0900

    [feat] 기본 명령어 구현

---
+++ include/commands.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+void cmd_pwd(void);
+void cmd_cd(int argc, char *argv[]);
+void cmd_ls(int argc, char *argv[]);
+void cmd_mkdir(int argc, char *argv[]);
+void cmd_cat(int argc, char *argv[]);
+
+#endif
