c06c69c3b8a1cf24f1ee8f60c24fbdc16326ef41
issuejong <lsj63223@naver.com>
2026-05-21 10:27:51 +0900

    [Refactor] 코드 위치 수정

--- include/commands.h
+++ include/commands.h
@@ -1,17 +1,17 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+/* Juheon */
+void cmd_cd(int argc, char *argv[]);
+void cmd_ls(int argc, char *argv[]);
+void cmd_mkdir(int argc, char *argv[]);
+void cmd_cat(int argc, char *argv[]);
+
 /* Jihwan */
 void cmd_chown(const char *owner, const char *path);
 void cmd_grep(const char *keyword, const char *path, int showLineNumber);
 void cmd_mv(const char *srcPath, const char *destPath);
 void cmd_rm(const char *path, int recursive, int force);
 void cmd_pwd(void);
-
-/* Juheon */
-void cmd_cd(int argc, char *argv[]);
-void cmd_ls(int argc, char *argv[]);
-void cmd_mkdir(int argc, char *argv[]);
-void cmd_cat(int argc, char *argv[]);
 
 #endif
--- src/commands.c
+++ src/commands.c
@@ -6,22 +6,6 @@
 #define MAX_CONTENT 4096
 #define MAX_INPUT 1024
   
-/* Jihwan */
-void cmd_chown(const char *owner, const char *path) {
-    printf("[chown] owner=%s, path=%s\n", owner, path);
-}
-
-void cmd_grep(const char *keyword, const char *path, int showLineNumber) {
-    printf("[grep] keyword=%s, path=%s, -n=%d\n", keyword, path, showLineNumber);
-}
-
-void cmd_mv(const char *srcPath, const char *destPath) {
-    printf("[mv] src=%s, dest=%s\n", srcPath, destPath);
-}
-
-void cmd_rm(const char *path, int recursive, int force) {
-    printf("[rm] path=%s, recursive=%d, force=%d\n", path, recursive, force);
-  
 /* Juheon */
 typedef enum {
     NODE_DIR,
@@ -425,3 +409,20 @@ void cmd_cat(int argc, char *argv[]) {
 
     cat_read_file(argv[1], 0);
 }
+
+/* Jihwan */
+void cmd_chown(const char *owner, const char *path) {
+    printf("[chown] owner=%s, path=%s\n", owner, path);
+}
+
+void cmd_grep(const char *keyword, const char *path, int showLineNumber) {
+    printf("[grep] keyword=%s, path=%s, -n=%d\n", keyword, path, showLineNumber);
+}
+
+void cmd_mv(const char *srcPath, const char *destPath) {
+    printf("[mv] src=%s, dest=%s\n", srcPath, destPath);
+}
+
+void cmd_rm(const char *path, int recursive, int force) {
+    printf("[rm] path=%s, recursive=%d, force=%d\n", path, recursive, force);
+  
